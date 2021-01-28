941e5b33a113cba92dda26728b151b2a1303aa4a
Easycker <1778466+Easycker@users.noreply.github.com>
2021-01-28 22:31:27 +0800

    Add sin() and cos() example to the collection.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,12 @@
-let diameter;
-let angle = 0;
-
 function setup() {
-    createCanvas(710, 400);
-    diameter = height - 10;
-    noStroke();
-    fill(255, 204, 0);
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background(0);
-
-    let d1 = 10 + (sin(angle) * diameter) / 2 + diameter / 2;
-    let d2 = 10 + (sin(angle + PI / 2) * diameter) / 2 + diameter / 2;
-    let d3 = 10 + (sin(angle + PI) * diameter) / 2 + diameter / 2;
-
-    ellipse(0, height / 2, d1, d1);
-    ellipse(width / 2, height / 2, d2, d2);
-    ellipse(width, height / 2, d3, d3);
-
-    angle += 0.02;
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
